c8d2ea864dc6f7324926c3bdba337c202c4b67dc
bili <panning20@gmail.com>
2015-11-05 09:34:53 +0800

    change to online script

--- static/js/index.js
+++ static/js/index.js
@@ -3,9 +3,6 @@ var mapContext = mapCanvas.getContext('2d');
 mapCanvas.width = window.innerWidth;
 mapCanvas.height = window.innerHeight;
 
-function log() {
-	console && console.log.apply(console, arguments);
-}
 var Human = function(ctx) {
 	this._x = 0;
 	this._y = 0;
@@ -15,7 +12,7 @@ var Human = function(ctx) {
 	img.src = this._res;
 	var _self = this;
 	img.onload = function() {
-		log('loading', _self._res, img.width, img.height);
+		_.log('loading', _self._res, img.width, img.height);
 		ctx.drawImage(img, 100, 100, img.width, img.height);
 	};
 	return this;
