af300028edf19e1beee3abce10e4c23851692c51
bili <panning20@gmail.com>
2015-11-05 16:30:59 +0800

    clear debug output

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,14 @@
-DEBUG = true;
+_.log.DEBUG = true;
 
 var KEY = {
 	'UP': 38,
 	'DOWN': 40,
 	'LEFT': 37,
-	'RIGHT': 39
+	'RIGHT': 39,
+	'W': 87,
+	'S': 83,
+	'A': 65,
+	'D': 68
 };
 var cvs = document.querySelector('#map');
 var ctx = cvs.getContext('2d');
@@ -31,23 +35,32 @@ window.addEventListener('resize', function(e) {
 	cvs.height = window.innerHeight;
 });
 document.addEventListener('keydown', function(e) {
-	if (DEBUG) _.log('Event keyDown triggered', e.keyCode);
+	_.log('Event keyDown triggered', e.keyCode);
 	switch(e.keyCode) {
 		case KEY.LEFT:
-			if (DEBUG) _.log('Move to', man._x - 1, man._y)
+			man.moveTo(man._x -1, man._y);
+			break;
+		case KEY.A:
 			man.moveTo(man._x -1, man._y);
 			break;
 		case KEY.RIGHT:
-			if (DEBUG) _.log('Move to', man._x + 1, man._y)
+			man.moveTo(man._x + 1, man._y);
+			break;
+		case KEY.D:
 			man.moveTo(man._x + 1, man._y);
 			break;
 		case KEY.UP:
-			if (DEBUG) _.log('Move to', man._x, man._y - 1)
+			man.moveTo(man._x, man._y - 1);
+			break;
+		case KEY.W:
 			man.moveTo(man._x, man._y - 1);
 			break;
 		case KEY.DOWN:
-			if (DEBUG) _.log('Move to', man._x, man._y + 1)
+			man.moveTo(man._x, man._y + 1);
+			break;
+		case KEY.S:
 			man.moveTo(man._x, man._y + 1);
 			break;
 	}
+	_.log('Move to', man._x, man._y)
 }, false);
