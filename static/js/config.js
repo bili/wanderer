5efdeae69bac8edbca017985c06a949d077d784b
bili <panning20@gmail.com>
2015-11-05 16:33:43 +0800

    add config file

---
+++ static/js/config.js
@@ -0,0 +1,12 @@
+_.log.DEBUG = true;
+
+var KEY = {
+	'UP': 38,
+	'DOWN': 40,
+	'LEFT': 37,
+	'RIGHT': 39,
+	'W': 87,
+	'S': 83,
+	'A': 65,
+	'D': 68
+};
--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,3 @@
-_.log.DEBUG = true;
-
-var KEY = {
-	'UP': 38,
-	'DOWN': 40,
-	'LEFT': 37,
-	'RIGHT': 39,
-	'W': 87,
-	'S': 83,
-	'A': 65,
-	'D': 68
-};
 var cvs = document.querySelector('#map');
 var ctx = cvs.getContext('2d');
 cvs.width = window.innerWidth;
